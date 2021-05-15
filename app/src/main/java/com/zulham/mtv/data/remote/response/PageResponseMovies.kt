79b23d97e6cc78af5b2d95c342d41ec2951cff0a
ZANR <62210020+Z4nR@users.noreply.github.com>
2021-05-15 14:34:36 +0700

    submission 3 jetpack paging final

--- app/src/main/java/com/zulham/mtv/data/remote/response/PageResponseMovies.kt
+++ app/src/main/java/com/zulham/mtv/data/remote/response/PageResponseMovies.kt
@@ -4,17 +4,9 @@ import com.google.gson.annotations.SerializedName
 
 data class PageResponseMovies(
 
-	@field:SerializedName("page")
-	val page: Int? = null,
-
-	@field:SerializedName("total_pages")
-	val totalPages: Int? = null,
-
 	@field:SerializedName("results")
-	val results: List<ResultsMovies>? = null,
+	val results: List<ResultsMovies>? = null
 
-	@field:SerializedName("total_results")
-	val totalResults: Int? = null
 )
 
 data class ResultsMovies(
